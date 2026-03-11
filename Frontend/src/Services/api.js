754d4251c787ff971c8c0773c0ce57c72c7ea536
Nikhil Bompelly <bompellynikhil13@gmail.com>
2026-03-11 10:02:26 +0530

    Update api.js

--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api"
+  baseURL: "https://nextskill-backend.onrender.com"
 });
 
 export const getCourses = () => API.get("/courses");
